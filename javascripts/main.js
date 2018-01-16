69d0338018d6345dbd4a03953344643e309336eb
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-16 15:34:54 -0600

    have dog brands name in DOM, am not running grunt cant figure out whats wrong with it when i set it up weeks ago

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,31 @@
 "use strict";
 
-let displayInfo = document.getElementById("displayInfo");
 
-function getFoods(data) {
-    let request = new XMLHttpRequest();
-    request.addEventListener("load", () => {
-        data = JSON.parse(this.Responsetext);
-        let brand = data.brand;
-    });
+function getFoods() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "dogfood.json", true)
+    
+    request.onload = function () {
+        if (this.status === 200) {
+            let data = JSON.parse(this.responseText);
+            console.log('data',data.dog_brands );
+            getBrands(data.dog_brands)
+            
+            
+        }
+    }
+    request.send()
+    function getBrands(dogArry) {
+        dogArry.forEach( brand => {
+            let displayInfo = document.getElementById("displayBrands");
+            displayInfo.innerHTML += `<h3>${brand.name}</h3>`;
+
+            brand.types.forEach(item =>{
+                
+            })
+        });
+    }
+   
 };
+
+getFoods()
